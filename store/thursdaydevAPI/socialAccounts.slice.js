2fe65cdceb9a73578ba1a8d517cf09eabad3a019
Crowdbotics <engineering@crowdbotics.com>
2023-06-27 14:00:47 +0000

    Update store.

--- store/thursdaydevAPI/socialAccounts.slice.js
+++ store/thursdaydevAPI/socialAccounts.slice.js
@@ -1,32 +1,32 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const rest_auth_registration_verify_email_create = createAsyncThunk(
-  "verifyEmails/rest_auth_registration_verify_email_create",
+export const modules_social_auth_socialaccounts_list = createAsyncThunk(
+  "socialAccounts/modules_social_auth_socialaccounts_list",
   async payload => {
-    const response = await apiService.rest_auth_registration_verify_email_create(
+    const response = await apiService.modules_social_auth_socialaccounts_list(
       payload
     )
     return response.data
   }
 )
 const initialState = { entities: [], api: { loading: "idle", error: null } }
-const verifyEmailsSlice = createSlice({
-  name: "verifyEmails",
+const socialAccountsSlice = createSlice({
+  name: "socialAccounts",
   initialState,
   reducers: {},
   extraReducers: {
-    [rest_auth_registration_verify_email_create.pending]: (state, action) => {
+    [modules_social_auth_socialaccounts_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [rest_auth_registration_verify_email_create.fulfilled]: (state, action) => {
+    [modules_social_auth_socialaccounts_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
+        state.entities = action.payload
         state.api.loading = "idle"
       }
     },
-    [rest_auth_registration_verify_email_create.rejected]: (state, action) => {
+    [modules_social_auth_socialaccounts_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
@@ -35,6 +35,6 @@ const verifyEmailsSlice = createSlice({
   }
 })
 export default {
-  rest_auth_registration_verify_email_create,
-  slice: verifyEmailsSlice
+  modules_social_auth_socialaccounts_list,
+  slice: socialAccountsSlice
 }

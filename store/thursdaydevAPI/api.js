fe815dec95168a86862105cecd6e437abef6eee6
Hesbon Kiptoo <hesbon.kiptoo@crowdbotics.com>
2023-06-27 16:59:20 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/thursdaydevAPI/api.js
+++ store/thursdaydevAPI/api.js
@@ -1,73 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const thursdaydevAPI = axios.create({
   baseURL: "https://thursday-dev-94192.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return thursdaydevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_job_list(payload) {
-  return thursdaydevAPI.get(`/api/v1/job/`)
+  return thursdaydevAPI.get(`/api/v1/job/`);
 }
+
 function api_v1_job_create(payload) {
-  return thursdaydevAPI.post(`/api/v1/job/`, payload.data)
+  return thursdaydevAPI.post(`/api/v1/job/`, payload.data);
 }
+
 function api_v1_job_retrieve(payload) {
-  return thursdaydevAPI.get(`/api/v1/job/${payload.id}/`)
+  return thursdaydevAPI.get(`/api/v1/job/${payload.id}/`);
 }
+
 function api_v1_job_update(payload) {
-  return thursdaydevAPI.put(`/api/v1/job/${payload.id}/`, payload.data)
+  return thursdaydevAPI.put(`/api/v1/job/${payload.id}/`, payload.data);
 }
+
 function api_v1_job_partial_update(payload) {
-  return thursdaydevAPI.patch(`/api/v1/job/${payload.id}/`, payload.data)
+  return thursdaydevAPI.patch(`/api/v1/job/${payload.id}/`, payload.data);
 }
+
 function api_v1_job_destroy(payload) {
-  return thursdaydevAPI.delete(`/api/v1/job/${payload.id}/`)
+  return thursdaydevAPI.delete(`/api/v1/job/${payload.id}/`);
 }
+
 function api_v1_login_create(payload) {
-  return thursdaydevAPI.post(`/api/v1/login/`, payload.data)
+  return thursdaydevAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return thursdaydevAPI.post(`/api/v1/signup/`, payload.data)
+  return thursdaydevAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return thursdaydevAPI.post(`/rest-auth/login/`, payload.data)
+  return thursdaydevAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return thursdaydevAPI.get(`/rest-auth/logout/`)
+  return thursdaydevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return thursdaydevAPI.post(`/rest-auth/logout/`)
+  return thursdaydevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return thursdaydevAPI.post(`/rest-auth/password/change/`, payload.data)
+  return thursdaydevAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return thursdaydevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return thursdaydevAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return thursdaydevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return thursdaydevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return thursdaydevAPI.post(`/rest-auth/registration/`, payload.data)
+  return thursdaydevAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return thursdaydevAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return thursdaydevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return thursdaydevAPI.get(`/rest-auth/user/`)
+  return thursdaydevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return thursdaydevAPI.put(`/rest-auth/user/`, payload.data)
+  return thursdaydevAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return thursdaydevAPI.patch(`/rest-auth/user/`, payload.data)
+  return thursdaydevAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_job_list,
@@ -89,4 +112,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};

d6fd4997d33ef271cbe5ea2cb4e8d7ce8ae72e51
crowdbotics-dev <engineering@crowdbotics.com>
2023-06-27 12:55:54 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -2,5 +2,4 @@ from django.conf import settings
 from django.db import models
 class Job(models.Model):
     'Generated Model'
-    title = models.CharField(max_length=256,)
-    user = models.ForeignKey("users.User",on_delete=models.CASCADE,null=True,blank=True,related_name="job_user",)
+    user = models.ForeignKey("users.User",null=True,blank=True,on_delete=models.CASCADE,related_name="job_user",)
